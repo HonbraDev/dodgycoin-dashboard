b6f820581a696aa9582a1ff1c8a5def0bb00db8e
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-04-14 18:21:54 +0200

    Updated cache types

--- utils/types.ts
+++ utils/types.ts
@@ -3,7 +3,13 @@ import { User as DogeHouseUser } from "@dogehouse/kebab";
 export type User = {
   monies: number;
   github?: string;
-  cache?: DogeHouseUser;
+  cache: {
+    avatarUrl: DogeHouseUser["avatarUrl"];
+    bio: DogeHouseUser["bio"];
+    displayName: DogeHouseUser["displayName"];
+    id: DogeHouseUser["id"];
+    username: DogeHouseUser["username"];
+  };
 };
 
 export type AccountList = Record<string, User>;
