0980e82644e05ea139dc2477480ac26a77c920a7
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-04-17 15:11:14 +0200

    Removed DogeHouse profile API thing until they implement anonymous APIs

--- pages/api/top.ts
+++ pages/api/top.ts
@@ -1,12 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import supabaseAdmin from "../../utils/supabaseadmin";
-import dogehouseClient from "../../utils/dogehouseClient";
 import { User } from "../../utils/types";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const supabase = supabaseAdmin();
-    const client = await dogehouseClient();
     const { data: rawData, error } = await supabase
       .from("users")
       .select("*")
@@ -14,15 +12,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       .limit(10);
     const data = rawData as User[];
     if (error) throw error;
-    const dogehouseUsers = await Promise.all(
-      data.map((user) => client.query.getUserProfile(user.id))
-    );
-    res.status(200).json(
-      data.map((user, index) => ({
-        dogehouse: dogehouseUsers[index],
-        ...user,
-      }))
-    );
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json(error);
   }
